bb4cec1d27ed851acb89876d7bc424d67264148f
Chethan V <87270631+ChethanV2012@users.noreply.github.com>
2025-06-11 10:49:56 +0530

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -6,7 +6,6 @@ import contact_image from './contact_image.png'
 import about_image from './about_image.png'
 import logo from './logo.svg'
 import Logo from './Logo.png'
-
 import dropdown_icon from './dropdown_icon.svg'
 import menu_icon from './menu_icon.svg'
 import cross_icon from './cross_icon.png'
